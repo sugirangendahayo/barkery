8f60a89140d0f9883a4b8c6ed9f23271c5da841a
Volonte Rwicha <volonterwicha2004@gmail.com>
2025-05-29 20:00:45 +0200

    update files

--- bark/src/App.jsx
+++ bark/src/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Navbar from "./Navbar";
 import About from "./About";
 import Footer from "./Footer";
-import Navbar from "./Navbar";
 import Service from "./Service";
 
 export default function App() {
-  return <div>
-    <Navbar/>
-    <About/>
-    <Service/>
-    <Footer/>
-  </div>;
-
+  return (
+    <div>
+      <Navbar />
+      <About />
+      <Service />
+      <Footer />
+    </div>
+  );
+}
