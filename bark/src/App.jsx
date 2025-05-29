dd7812f034841050876706aa29cd05d5994a4a3e
Volonte Rwicha <volonterwicha2004@gmail.com>
2025-05-29 19:45:52 +0200

    Add about file

--- bark/src/App.jsx
+++ bark/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import About from "./About";
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <About />
     </div>
   );
 }
