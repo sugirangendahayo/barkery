e173ccb6ccdda2f3d2a77d33f06bc740423f39bf
sugirangendahayo <Sugirangendahayo835@gmail.com>
2025-05-29 05:51:17 -1200

    Add file

--- bark/src/App.jsx
+++ bark/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Service from "./Service";
 
 export default function App() {
   return <div>
     <Navbar/>
+    <Service/>
     <Footer/>
   </div>;
 }
