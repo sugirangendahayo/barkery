12fde39b92c7168e9165c42d45aedeff77398e99
Volonte Rwicha <volonterwicha2004@gmail.com>
2025-05-29 19:38:34 +0200

    Add bracket

--- bark/src/App.jsx
+++ bark/src/App.jsx
@@ -3,9 +3,10 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function App() {
-  return <div>
-    <Navbar />
-    <Footer/>
-  </div>;
-
-
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
